f7171d62faefb3730685bc596553ca373cdcd1f8
peter rader <raderp841@gmail.com>
2019-10-09 13:33:16 -0400

    update for aws

--- spaceBoundAPI/spaceBoundAPI/Models/Currencies.cs
+++ spaceBoundAPI/spaceBoundAPI/Models/Currencies.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,13 @@ namespace spaceBoundAPI.Models
 {
     public class Currencies
     {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string CurrencyName { get; set; }
         [Key]
         public string CurrencyCode { get; set; }
         public decimal ExchangeRate { get; set; }
         public DateTime LastUpdatedTime { get; set; }
+
     }
 }
--- spaceBoundAPI/spaceBoundAPI/Program.cs
+++ spaceBoundAPI/spaceBoundAPI/Program.cs
@@ -19,6 +19,9 @@ namespace spaceBoundAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+               // .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>();
     }
 }
